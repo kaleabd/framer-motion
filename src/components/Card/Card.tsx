8fb95212e41853afb1715ef943ee93405a307f62
kaleabd <kaleabdereje94@gmail.com>
2023-05-04 10:32:49 +0300

    i've never thought that dragging and moving div's around this easy thanks for drag and drag constraints to control them

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,27 +3,45 @@ import Black from '../../assets/black.png'
 import Blue from '../../assets/blue.png'
 import Green from '../../assets/green.png'
 import Purple from '../../assets/purp.png'
+import { motion } from 'framer-motion'
 
 
 function Card() {
   return (
     <div className='main-container'>
-        <div className='container'>
+        <motion.div
+        whileHover={{
+            scale: [1.02, 0.8, 1.2],
+        }}
+        whileTap={{ background: "#f37f14"} }
+        className='container'>
             <img src={Black} alt="" />
             <h2>some texts here</h2>
-        </div>
-        <div className='container'>
+        </motion.div>
+        <motion.div 
+        drag
+        dragConstraints={{
+            top: -100,
+            left: -100,
+            right: 100,
+            bottom: 100,
+        }}
+        className='container'>
             <img src={Blue} alt="" />
             <h2>some texts here</h2>
-        </div>
-        <div className='container'>
+        </motion.div>
+        <motion.div
+        whileHover={{scale: 1.1}}
+        className='container'>
             <img src={Green} alt="" />
             <h2>some texts here</h2>
-        </div>
-        <div className='container'>
+        </motion.div>
+        <motion.div
+        whileHover={{scale: 1.1}}
+        className='container'>
             <img src={Purple} alt="" />
             <h2>some texts here</h2>
-        </div>
+        </motion.div>
 
     </div>
   )
