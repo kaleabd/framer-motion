a13cd4fa8636d2441d01143c6bd7c67d2d7eebdb
kaleabd <kaleabdereje94@gmail.com>
2023-05-09 06:40:17 +0300

    added the variants to the route animations

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -2,13 +2,19 @@ import Navbar from '../../components/Navbar/Navbar'
 import SlideShow from '../../components/SlideShow/SlideShow'
 import { motion } from 'framer-motion'
 
+const variants = {
+    initial: {opacity: 0, y: -100},
+    animate: {opacity: 1, y: 0},
+    exit : {opacity: 0, y: 100}
+}
 
 const About = () => {
   return (
     <motion.div
-    initial={{opacity: 0, y: -100}}
-    animate={{opacity: 1, y: 0}}
-    exit={{opacity: 0, y: 100}}
+    variants={variants}
+    initial="initial"
+    animate="animate"
+    exit="exit"
     >
         <Navbar />
         <h2>slide show, fashion show! xD</h2>
